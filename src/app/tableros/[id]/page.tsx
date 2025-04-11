aff039f0f0fc9a44ce10e98c0f06674e9f73650b
Kenn Marcucci <marcucci5783@gmail.com>
2025-04-11 16:06:20 -0500

    feat: add DiagramaGantt component and integrate into TableroDetalle for Gantt chart visualization

--- src/app/tableros/[id]/page.tsx
+++ src/app/tableros/[id]/page.tsx
@@ -9,6 +9,7 @@ import Scrum from '@/components/Scrum'
 import Kanban from '@/components/Kanban'
 import { CustomSwitch } from '@/components/CustomSwitch'
 import FilterTaskForm from '@/components/FilterTaskForm'
+import DiagramaGantt from '@/components/DiagramaGantt'
 
 export default function TableroDetalle() {
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
@@ -29,83 +30,88 @@ export default function TableroDetalle() {
   };
 
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="bg-gray-100 flex flex-col w-full p-10">
-        <div className="flex justify-between items-center mb-6">
-          <div>
-            <h1 className="text-2xl font-bold text-gray-900">
-              Proyecto Alpha
-            </h1>
-            <p className="text-sm text-gray-500">
-              Tablero del proyecto principal
-            </p>
-          </div>
-          <div className="flex items-center space-x-4">
-            <select
-              className="px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
-              value={boardType}
-              onChange={(e) =>
-                setBoardType(e.target.value as 'kanban' | 'scrum')
-              }
-            >
-              <option value="kanban">Kanban</option>
-              <option value="scrum">Scrum</option>
-            </select>
-
-            <button
-              onClick={() => setIsFilterModalOpen(true)}
-              className="border-blue-600 text-blue-600 hover:bg-blue-700 hover:text-white duration-150 px-4 py-2 rounded-md border whitespace-nowrap"
-            >
-              Filtrar
-            </button>
-
-            <button
-              onClick={() => setIsCreateModalOpen(true)}
-              className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 whitespace-nowrap"
-            >
-              {boardType === 'kanban' ? 'Nueva tarea' : 'Nueva historia'}
-            </button>
-          </div>
+    <main className="bg-gray-100 flex flex-col p-10 ml-64 min-h-screen">
+      <div className="flex justify-between items-center mb-6">
+        <div>
+          <h1 className="text-2xl font-bold text-gray-900">
+            Proyecto Alpha
+          </h1>
+          <p className="text-sm text-gray-500">
+            Tablero del proyecto principal
+          </p>
         </div>
+        <div className="flex items-center space-x-4">
+          <select
+            className="px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50"
+            value={boardType}
+            onChange={(e) =>
+              setBoardType(e.target.value as 'kanban' | 'scrum')
+            }
+          >
+            <option value="kanban">Kanban</option>
+            <option value="scrum">Scrum</option>
+          </select>
 
-        {/* const [viewType, setViewType] = useState<"Tablero" | "Diagrama de Gantt">("Tablero") */}
-        <CustomSwitch value={viewType} onChange={setViewType} />
+          <button
+            onClick={() => setIsFilterModalOpen(true)}
+            className="border-blue-600 text-blue-600 hover:bg-blue-700 hover:text-white duration-150 px-4 py-2 rounded-md border whitespace-nowrap"
+          >
+            Filtrar
+          </button>
 
-        {boardType === 'kanban' ? <Kanban /> : <Scrum />}
+          <button
+            onClick={() => setIsCreateModalOpen(true)}
+            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 whitespace-nowrap"
+          >
+            {boardType === 'kanban' ? 'Nueva tarea' : 'Nueva historia'}
+          </button>
+        </div>
+      </div>
 
-        <Modal
-          isOpen={isCreateModalOpen}
-          onClose={() => setIsCreateModalOpen(false)}
-          title={
-            boardType === 'kanban' ? 'Crear nueva tarea' : 'Crear nueva historia'
-          }
-        >
-          {boardType === 'kanban' ? (
-            <CreateTaskForm
-              onSubmit={handleCreateItem}
-              onCancel={() => setIsCreateModalOpen(false)}
-            />
-          ) : (
-            <CreateUserStoryForm
-              onSubmit={handleCreateItem}
-              onCancel={() => setIsCreateModalOpen(false)}
-            />
-          )}
-        </Modal>
+      {/* const [viewType, setViewType] = useState<"Tablero" | "Diagrama de Gantt">("Tablero") */}
+      <CustomSwitch value={viewType} onChange={setViewType} />
 
-        {/* Modal para Filtros */}
-        <Modal
-          isOpen={isFilterModalOpen}
-          onClose={() => setIsFilterModalOpen(false)}
-          title="Filtros"
-        >
-          <FilterTaskForm
-            onSubmit={handleFilter}
-            onCancel={() => setIsFilterModalOpen(false)}
+      {
+        viewType == "Tablero" ? (
+          <>{boardType === 'kanban' ? <Kanban /> : <Scrum />}</>
+        ) : (
+          <>
+            <DiagramaGantt />
+          </>
+        )
+      }
+
+      <Modal
+        isOpen={isCreateModalOpen}
+        onClose={() => setIsCreateModalOpen(false)}
+        title={
+          boardType === 'kanban' ? 'Crear nueva tarea' : 'Crear nueva historia'
+        }
+      >
+        {boardType === 'kanban' ? (
+          <CreateTaskForm
+            onSubmit={handleCreateItem}
+            onCancel={() => setIsCreateModalOpen(false)}
           />
-        </Modal>
-      </main>
-    </div>
+        ) : (
+          <CreateUserStoryForm
+            onSubmit={handleCreateItem}
+            onCancel={() => setIsCreateModalOpen(false)}
+          />
+        )}
+      </Modal>
+
+      {/* Modal para Filtros */}
+      <Modal
+        isOpen={isFilterModalOpen}
+        onClose={() => setIsFilterModalOpen(false)}
+        title="Filtros"
+      >
+        <FilterTaskForm
+          onSubmit={handleFilter}
+          onCancel={() => setIsFilterModalOpen(false)}
+        />
+      </Modal>
+    </main>
   )
 } 
--- src/app/tableros/page.tsx
+++ src/app/tableros/page.tsx
@@ -57,74 +57,71 @@ export default function TablerosPage() {
   };
 
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="bg-gray-100 w-full flex flex-col p-10">
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="text-2xl font-bold text-gray-900">Tableros</h1>
-          <div className='flex gap-2'>
-            <button
-              onClick={() => setIsFilterModalOpen(true)}
-              className="border-blue-600 text-blue-600 hover:bg-blue-700 hover:text-white duration-150 px-4 py-2 rounded-md border whitespace-nowrap"
-            >
-              Filtrar
-            </button>
-            <button
-              onClick={() => setIsCreateModalOpen(true)}
-              className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 duration-150 whitespace-nowrap"
-            >
-              Crear tablero
-            </button>
-          </div>
+    <main className="bg-gray-100 flex flex-col p-10 ml-64 min-h-screen">
+      <div className="flex justify-between items-center mb-8">
+        <h1 className="text-2xl font-bold text-gray-900">Tableros</h1>
+        <div className='flex gap-2'>
+          <button
+            onClick={() => setIsFilterModalOpen(true)}
+            className="border-blue-600 text-blue-600 hover:bg-blue-700 hover:text-white duration-150 px-4 py-2 rounded-md border whitespace-nowrap"
+          >
+            Filtrar
+          </button>
+          <button
+            onClick={() => setIsCreateModalOpen(true)}
+            className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 duration-150 whitespace-nowrap"
+          >
+            Crear tablero
+          </button>
         </div>
+      </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {
-            boards.map(board =>
-              <Link
-                key={board.id}
-                href={`/tableros/${board.id}`}
-                className="block bg-white p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow"
-              >
-                <h3 className="text-lg font-semibold text-gray-900">
-                  {board.title}
-                </h3>
-                <p className="text-gray-600 mt-1">{board.description}</p>
-                <div className="mt-4 flex items-center">
-                  <span className={`px-2 py-1 text-xs rounded ${board.type === 'kanban'
-                    ? 'bg-green-100 text-green-800' : 'bg-blue-100 text-blue-800'}`}>
-                    {board.type.toUpperCase()}
-                  </span>
-                </div>
-              </Link>
-            )
-          }
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {
+          boards.map(board =>
+            <Link
+              key={board.id}
+              href={`/tableros/${board.id}`}
+              className="block bg-white p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow"
+            >
+              <h3 className="text-lg font-semibold text-gray-900">
+                {board.title}
+              </h3>
+              <p className="text-gray-600 mt-1">{board.description}</p>
+              <div className="mt-4 flex items-center">
+                <span className={`px-2 py-1 text-xs rounded ${board.type === 'kanban'
+                  ? 'bg-green-100 text-green-800' : 'bg-blue-100 text-blue-800'}`}>
+                  {board.type.toUpperCase()}
+                </span>
+              </div>
+            </Link>
+          )
+        }
+      </div>
 
-        {/* Modal para Tableros */}
-        <Modal
-          isOpen={isCreateModalOpen}
-          onClose={() => setIsCreateModalOpen(false)}
-          title="Crear nuevo tablero"
-        >
-          <CreateBoardForm
-            onSubmit={handleCreateBoard}
-            onCancel={() => setIsCreateModalOpen(false)}
-          />
-        </Modal>
+      {/* Modal para Tableros */}
+      <Modal
+        isOpen={isCreateModalOpen}
+        onClose={() => setIsCreateModalOpen(false)}
+        title="Crear nuevo tablero"
+      >
+        <CreateBoardForm
+          onSubmit={handleCreateBoard}
+          onCancel={() => setIsCreateModalOpen(false)}
+        />
+      </Modal>
 
-        {/* Modal para Filtros */}
-        <Modal
-          isOpen={isFilterModalOpen}
-          onClose={() => setIsFilterModalOpen(false)}
-          title="Filtros"
-        >
-          <FilterProjectForm
-            onSubmit={handleFilter}
-            onCancel={() => setIsFilterModalOpen(false)}
-          />
-        </Modal>
-      </main>
-    </div>
+      {/* Modal para Filtros */}
+      <Modal
+        isOpen={isFilterModalOpen}
+        onClose={() => setIsFilterModalOpen(false)}
+        title="Filtros"
+      >
+        <FilterProjectForm
+          onSubmit={handleFilter}
+          onCancel={() => setIsFilterModalOpen(false)}
+        />
+      </Modal>
+    </main>
   );
 } 
