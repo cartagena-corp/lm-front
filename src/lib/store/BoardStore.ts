0d23968ec38686491d953b7bdba6157a28716396
Kenn Marcucci <marcucci5783@gmail.com>
2025-04-18 17:23:21 -0500

    feat: enhance setBoards function to support filtering options for project retrieval

--- src/lib/store/BoardStore.ts
+++ src/lib/store/BoardStore.ts
@@ -1,4 +1,4 @@
-import { ProjectProps } from '../types/types'
+import { FilterProjectProps, ProjectProps } from '../types/types'
 import { create } from 'zustand'
 
 interface BoardProps {
@@ -11,7 +11,7 @@ interface BoardProps {
 
 interface BoardState {
    boards: BoardProps
-   setBoards: (token: string) => Promise<void>
+   setBoards: (token: string, filters?: FilterProjectProps) => Promise<void>
    createBoard: (
       token: string,
       boardData: { name: string, description: string, startDate: string, endDate: string, status: number }
@@ -28,34 +28,48 @@ export const useBoardStore = create<BoardState>((set) => ({
       number: 0,
       size: 10
    },
-   setBoards: async (token: string) => {
+   setBoards: async (token, filters) => {
       try {
-         const response = await fetch(`${API_URL}${process.env.NEXT_PUBLIC_GET_PROJECTS}`, {
+         // Build query string with filters
+         const params = new URLSearchParams()
+         if (filters) {
+            if (filters.name) params.append('name', filters.name)
+            if (filters.status !== undefined && filters.status !== 0) params.append('status', filters.status.toString())
+            if (filters.createdBy) params.append('createdBy', filters.createdBy)
+            params.append('page', (filters.page ?? 0).toString())
+            params.append('size', (filters.size ?? 10).toString())
+
+            if (filters.sortBy) params.append('sortBy', filters.sortBy.id), params.append('direction', filters.direction)
+         }
+
+         const url = `${API_URL}${process.env.NEXT_PUBLIC_GET_PROJECTS}?${params.toString()}`
+
+         const response = await fetch(url, {
             method: 'GET',
             headers: {
-               "Content-Type": "application/json",
-               "Authorization": `Bearer ${token}`
-            }
+               'Content-Type': 'application/json',
+               'Authorization': `Bearer ${token}`,
+            },
          })
+
          if (!response.ok) {
-            console.error("Error al obtener los tableros", response.statusText)
+            console.error('Error al obtener los tableros', response.statusText)
             return
          }
 
          const data: BoardProps = await response.json()
-
+         console.log("data: ", data)
          set({
             boards: {
                content: data.content,
                totalElements: data.totalElements,
                totalPages: data.totalPages,
                number: data.number,
-               size: data.size
-            }
+               size: data.size,
+            },
          })
-      }
-      catch (error) {
-         console.error("Error en la solicitud", error)
+      } catch (error) {
+         console.error('Error en la solicitud', error)
       }
    },
    createBoard: async (token: string, boardData: { name: string, description: string, startDate: string, endDate: string, status: number }) => {
