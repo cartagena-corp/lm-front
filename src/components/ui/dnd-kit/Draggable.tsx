f93a833a5e49978f2d50cb02137a13accf316494
Kenn Marcucci <marcucci5783@gmail.com>
2025-04-29 10:24:15 -0500

    fix: update Draggable component to use empty string as default for styleClass prop

--- src/components/ui/dnd-kit/Draggable.tsx
+++ src/components/ui/dnd-kit/Draggable.tsx
@@ -5,15 +5,15 @@ import { CSS } from '@dnd-kit/utilities'
 interface DraggableProps {
    id: string
    children: React.ReactNode
-   styleClass: string | null
+   styleClass: string | ""
 }
 
-export function Draggable({ id, children, styleClass = null }: DraggableProps) {
+export function Draggable({ id, children, styleClass = "" }: DraggableProps) {
    const { attributes, listeners, setNodeRef, transform } = useDraggable({ id })
    const style = { transform: CSS.Translate.toString(transform) }
 
    return (
-      <div className={`${styleClass ? styleClass : "bg-white border"} rounded-md hover:shadow-lg active:duration-0 duration-150  p-4 cursor-grab active:cursor-grabbing select-none`} ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      <div className={styleClass} ref={setNodeRef} style={style} {...listeners} {...attributes}>
          {children}
       </div>
    )
