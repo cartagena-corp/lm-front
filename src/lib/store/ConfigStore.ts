a56256247d6b5fabe3f1bd25456185ba22c28979
Kenn Marcucci <marcucci5783@gmail.com>
2025-04-29 10:24:33 -0500

    refactor: update projectStatus type to ensure it is always an array and simplify fetch logic

--- src/lib/store/ConfigStore.ts
+++ src/lib/store/ConfigStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import { ConfigProjectStatusProps, ProjectConfigProps } from '../types/types'
 
 interface ConfigProjectState {
-   projectStatus: ConfigProjectStatusProps[] | null
+   projectStatus: ConfigProjectStatusProps[]
    projectConfig: ProjectConfigProps | null
 
    setConfig: () => Promise<void>
@@ -12,25 +12,18 @@ interface ConfigProjectState {
 const API_URL = process.env.NEXT_PUBLIC_CONFIG
 
 export const useConfigStore = create<ConfigProjectState>((set) => ({
-   projectStatus: null,
+   projectStatus: [],
    projectConfig: null,
    setConfig: async () => {
       try {
          const response = await fetch(`${API_URL}${process.env.NEXT_PUBLIC_GET_CONFIG_PROJECT_STATE}`, {
             method: 'GET',
-            headers: {
-               "Content-Type": "application/json",
-               "credentials": "include"
-            }
+            headers: { "Content-Type": "application/json", "credentials": "include" }
          })
 
          if (!response.ok) return console.error("Error al obtener las configuraciones de los estados globales de los proyectos", response.statusText)
-
          const data: ConfigProjectStatusProps[] = await response.json()
-
-         set({
-            projectStatus: data,
-         })
+         set({ projectStatus: data })
       }
       catch (error) {
          console.error("Error en la solicitud", error)
@@ -40,19 +33,12 @@ export const useConfigStore = create<ConfigProjectState>((set) => ({
       try {
          const response = await fetch(`${API_URL}${process.env.NEXT_PUBLIC_GET_CONFIG_GLOBAL_PROJECT}/${id}`, {
             method: 'POST',
-            headers: {
-               "Content-Type": "application/json",
-               "Authorization": `Bearer ${token}`
-            }
+            headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` }
          })
 
          if (!response.ok) return console.error("Error al obtener las configuraciones de los estados globales de los proyectos", response.statusText)
-
          const data: ProjectConfigProps = await response.json()
-
-         set({
-            projectConfig: data,
-         })
+         set({ projectConfig: data, })
       }
       catch (error) {
          console.error("Error en la solicitud", error)
