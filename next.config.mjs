e76d63b971a711e60a244b4e2d568b61e79ec127
Kenn Marcucci <marcucci5783@gmail.com>
2025-05-05 17:47:16 -0500

    feat: update image remotePatterns in next.config.mjs for improved local development support

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'http',
+        hostname: 'localhost',  // solo dominio
+        port: '8084',           // puerto separado
+        pathname: '/uploads/**' // opcional, patrones de ruta
+      },
+      {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        port: '',               // sin puerto
+        pathname: '/**'
       },
     ],
   },
 }
 
-export default nextConfig 
+export default nextConfig
