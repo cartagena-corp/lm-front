6993bac18f9f19762b239c1e8f041d81bbd5393c
Kenn Marcucci <marcucci5783@gmail.com>
2025-07-08 01:29:12 -0500

    fix: update border color in Droppable component for improved visibility when hovered

--- src/components/ui/dnd-kit/Droppable.tsx
+++ src/components/ui/dnd-kit/Droppable.tsx
@@ -10,7 +10,7 @@ interface DroppableProps {
 export function Droppable({ id, children, styleClass = '' }: DroppableProps) {
    const { isOver, setNodeRef } = useDroppable({ id })
    return (
-      <div className={`${styleClass} ${isOver ? 'border-blue-500' : 'border-transparent'} border-4 w-full rounded-md duration-300`} ref={setNodeRef}>
+      <div className={`${styleClass} ${isOver ? 'border-blue-400' : 'border-transparent'} border-4 w-full rounded-md duration-300`} ref={setNodeRef}>
          {children}
       </div>
    )
