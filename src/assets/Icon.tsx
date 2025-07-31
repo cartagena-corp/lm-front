e44428633ae6ff1b0bf9f518e291f82d9fe854ab
Kenn Marcucci <marcucci5783@gmail.com>
2025-07-31 14:10:23 -0500

    feat: Add ImportIcon component to Icon library

--- src/assets/Icon.tsx
+++ src/assets/Icon.tsx
@@ -303,3 +303,13 @@ export const ChatIAIcon = ({ size = 24, stroke = 1.5 }: IconProps) => {
       </svg>
    )
 }
+
+export const ImportIcon = ({ size = 24, stroke = 1.5 }: IconProps) => {
+   return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={"none"}>
+         <path d="M12 3v12" stroke="currentColor" strokeWidth={stroke} strokeLinecap="round" strokeLinejoin="round" />
+         <path d="m8 11 4 4 4-4" stroke="currentColor" strokeWidth={stroke} strokeLinecap="round" strokeLinejoin="round" />
+         <path d="M8 5H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-4" stroke="currentColor" strokeWidth={stroke} strokeLinecap="round" strokeLinejoin="round" />
+      </svg>
+   )
+}
