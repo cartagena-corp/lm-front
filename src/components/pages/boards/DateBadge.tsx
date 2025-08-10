e9ec628808ba3be063ca50fad4592059005ebb6e
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-10 13:01:12 -0500

    refactor: update labels in DateBadge for consistency and clarity

--- src/components/pages/boards/DateBadge.tsx
+++ src/components/pages/boards/DateBadge.tsx
@@ -17,13 +17,13 @@ const styles = {
     createdAt: {
         bg: "bg-purple-50",
         text: "text-purple-600",
-        label: "Creado",
+        label: "Fecha creación",
         Icon: ClockIcon
     },
     updatedAt: {
         bg: "bg-orange-50",
         text: "text-orange-600",
-        label: "Actualizado",
+        label: "Última actualización",
         Icon: ClockIcon
     },
 }
@@ -36,7 +36,10 @@ export default function DateBadge({ date, type }: DateBadgeProps) {
     }
     const IconComponent = styles[type].Icon
 
-    return (
+    if (!date) return null
+    if (isNaN(new Date(date).getTime())) return null
+
+    if (type === "startDate" || type === "endDate") return (
         <aside className="flex items-center gap-2">
             <span className={`${styles[type].bg} ${styles[type].text} rounded-md p-2`}>
                 <IconComponent size={16} strokeWidth={2} />
@@ -46,5 +49,20 @@ export default function DateBadge({ date, type }: DateBadgeProps) {
                 <p className="text-background-text font-light">{formatDate(date)}</p>
             </span>
         </aside>
+    )
+
+    if (type === "createdAt" || type === "updatedAt") return (
+        <hgroup className="flex justify-between items-center text-xs w-full gap-2">
+            <div className="flex items-center gap-2">
+                <span className={`${styles[type].text}`}>
+                    <IconComponent size={12} strokeWidth={2} />
+                </span>
+                <h6 className="font-semibold">{styles[type].label}</h6>
+            </div>
+            <span className="flex text-xs">
+
+                <p className="text-background-text font-light">{formatDate(date)}</p>
+            </span>
+        </hgroup>
     )
 }
