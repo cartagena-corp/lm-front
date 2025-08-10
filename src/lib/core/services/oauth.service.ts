b664153e7e95a6578956221242738f97dd8aa58c
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-09 21:50:08 -0500

    feat: refactor session initialization and token handling in oauth.service.ts

--- src/lib/core/services/oauth.service.ts
+++ src/lib/core/services/oauth.service.ts
@@ -1,55 +1,53 @@
-'use server'
+"use server"
 
-import { apiClient } from '@/lib/apiClient'
-import { API_ROUTES } from '@/lib/core/routes/oauth.route'
-import { useAuthStore } from '@stores/AuthStore'
+import { JwtPayload, UserProfile } from '@/lib/types/auth'
+import { logger } from '@/lib/types/Logger'
+import { jwtDecode } from 'jwt-decode'
 import { cookies } from 'next/headers'
 
-export async function initializeSessionAction() {
-    const { getProfileByToken } = useAuthStore()
-    const cookieStore = await cookies()
-
-    try {
-        const accessToken = cookieStore.get('accessToken')?.value
-        if (!accessToken) return { success: false }
-
-        const user = getProfileByToken({ token: accessToken })
-        return user
-    } catch (error) {
-        console.error("initializeSessionAction: Error al inicializar al usuario:", error)
-        return { success: false }
-    }
-}
-
-export async function logoutAction() {
+export async function initializeSessionAction(): Promise<UserProfile | null> {
     try {
         const cookieStore = await cookies()
-        const refreshToken = cookieStore.get('refreshToken')?.value
-        cookieStore.delete('accessToken')
-        cookieStore.delete('refreshToken')
-        await fetch(API_ROUTES.USER_LOGOUT, { method: 'POST', headers: { Cookie: `refreshToken=${refreshToken}` } })
-        return { success: true }
+        const token = cookieStore.get('accessToken')?.value
+        if (!token) {
+            logger.debug('No se encontró el token de acceso durante la inicialización de sesión')
+            return null
+        }
+
+        const decoded = jwtDecode<JwtPayload>(token)
+
+        logger.info('Sesión inicializada', { userId: decoded.sub || '', email: decoded.email || '' })
+        return {
+            permissions: decoded.permissions || [],
+            firstName: decoded.family_name || '',
+            lastName: decoded.given_name || '',
+            avatar: decoded.picture || '',
+            email: decoded.email || '',
+            role: decoded.role || '',
+            id: decoded.sub,
+        }
     } catch (error) {
-        console.error("logoutAction: Error al eliminar las cookies:", error)
-        return { success: false }
+        logger.error('Error al inicializar la sesión', error)
+        return null
     }
 }
 
-export async function refreshTokenAction(): Promise<{ success: boolean, accessToken?: string }> {
-    const cookieStore = await cookies()
-    const refreshToken = cookieStore.get('refreshToken')?.value
-
-    if (!refreshToken) return { success: false }
-
+export async function getProfileByToken({ token }: { token: string }): Promise<UserProfile | null> {
     try {
-        const res = await fetch(API_ROUTES.REFRESH_TOKEN, { method: 'POST', headers: { Cookie: `refreshToken=${refreshToken}` } })
-        if (!res.ok) throw new Error('Falló el refresco del token desde la API')
+        const decoded = jwtDecode<JwtPayload>(token)
 
-        const data = await res.json()
-        return { success: true, accessToken: data.accessToken }
-
+        logger.debug('Token decodificado correctamente', { userId: decoded.sub })
+        return {
+            permissions: decoded.permissions || [],
+            firstName: decoded.family_name || '',
+            lastName: decoded.given_name || '',
+            avatar: decoded.picture || '',
+            email: decoded.email || '',
+            role: decoded.role || '',
+            id: decoded.sub,
+        }
     } catch (error) {
-        console.error("refreshTokenAction:", error)
-        return { success: false }
+       logger.error('Error al decodificar el token', error)
+        return null
     }
 }
