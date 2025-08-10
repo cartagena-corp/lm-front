ada5469a771263620928c63ed295d4c102930741
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-10 13:53:46 -0500

    refactor: integrate useConfigStore for board status management in BoardCard

--- src/components/pages/boards/BoardCard.tsx
+++ src/components/pages/boards/BoardCard.tsx
@@ -1,4 +1,5 @@
 import type { BoardCardProps } from "@/lib/types/board"
+import { useConfigStore } from "@stores/ConfigStore"
 import avatar from "@public/img/avatar.png"
 import Badge from "@/components/ui/Badge"
 import { motion } from "motion/react"
@@ -7,12 +8,14 @@ import { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 
-
 export default function BoardCard({ board, index }: BoardCardProps) {
     const [isAnimated, setIsAnimated] = useState(false)
+    const { boardStates } = useConfigStore()
+
+    const status = boardStates.find(state => state.id === board.status)
     return (
         <motion.div className="bg-button-secondary-background rounded-md border-l-4 shadow-sm group"
-            // style={{ borderColor: status?.color ?? "#000000" }}
+            style={{ borderColor: `${status?.color}88` }}
             whileHover={{ y: -4, boxShadow: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)", transition: { type: "spring", stiffness: 300, damping: 20 } }}
             animate={isAnimated ? { opacity: 1, y: 0 } : { opacity: 1, y: 0, transition: { delay: index * 0.25 } }} onAnimationComplete={() => setIsAnimated(true)}
             transition={{ type: "spring", stiffness: 120, damping: 20 }} initial={{ opacity: 0, y: 20 }}
@@ -22,11 +25,10 @@ export default function BoardCard({ board, index }: BoardCardProps) {
                     <section className="flex flex-col gap-2">
                         <header className="flex justify-between items-center w-full gap-2">
                             <h5 className="group-hover:text-button-primary-text transition-colors font-semibold text-lg/tight line-clamp-3">{board.name}</h5>
-                            {/* <Badge hexColor={status?.color ?? "#000000"}>{status?.name ?? "Sin estado"}</Badge> */}
+                            <Badge hexColor={status?.color ?? "#000000"}>{status?.name ?? "Sin estado"}</Badge>
                         </header>
                         <p className="text-primary-border h-[3lh] line-clamp-3 text-xs">{board.description}</p>
                     </section>
-
                     <section className="flex items-center gap-2">
                         <picture className="bg-background-hover overflow-hidden rounded-full flex justify-center items-center relative flex-shrink-0 w-8 h-8">
                             <Image className={board.createdBy.picture ? "object-cover object-center" : "object-contain object-center p-1.5"}
