e6e62a4d6dbb5309b9e832a85b61da768d408457
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-09 21:49:22 -0500

    feat: enhance pagination types with detailed comments and structure

--- src/lib/types/pagination.d.ts
+++ src/lib/types/pagination.d.ts
@@ -1,19 +1,61 @@
+/**
+ * Interfaz que describe la estructura de la respuesta paginada del backend.
+ * @template T El tipo de los elementos en el array 'content'.
+ */
+
 export interface PaginatedResponse<T> {
-    totalElements: number
-    totalPages: number
-    number: number
-    content: T[]
-    size: number
+    totalElements: number               // * Cantidad total de elementos en todas las páginas.
+    totalPages: number                  // * Cantidad total de páginas disponibles.
+    number: number                      // * El número de la página actual (inicia en 0). 
+    size: number                        // * Cantidad de elementos por página. 
+
+    content: T[]                        // ? El listado de elementos para la página actual. 
 }
 
-export interface PaginatedState<T> {
-    setInitialData: (page: PaginatedResponse<T>) => void
-    addPage: (page: PaginatedResponse<T>) => void
-    setLoading: (loading: boolean) => void
-    totalElements: number
-    currentPage: number
-    totalPages: number
-    isLoading: boolean
-    reset: () => void
-    items: T[]
+export interface PUserProps {
+    firstName: string | null
+    lastName: string | null
+    picture: string | null
+    email: string
+    role: string
+    id: string
+}
+
+export interface PBoardProps {
+    createdBy: PUserProps               // * Viene sin campo Role
+    description: string
+    startDate: string
+    createdAt: string
+    updatedAt: string
+    endDate: string
+    status: number
+    name: string
+    id: string
+}
+
+export interface PIssueProps {
+    descriptions: {
+        title: string
+        text: string
+        id: string
+    }[]
+    estimatedTime: number | null
+    priority: number | null
+    reporterId: PUserProps              // * Viene sin campo Role
+    assignedId: PUserProps              // * Viene sin campo Role
+    status: number | null
+    type: number | null
+    createdAt: string
+    updatedAt: string
+    projectId: string
+    sprintId: string
+    title: string
+    id: string
+}
+
+export type ListComponentType = 'users' | 'boards' | 'issues'
+
+export interface PaginationFactoryProps {
+    data: PaginatedResponse<unknown> // * Los datos paginados. Usamos 'any' aquí porque el tipo exacto dependerá del valor de 'type'.
+    type: ListComponentType // * El tipo de lista que se debe renderizar.
 }
