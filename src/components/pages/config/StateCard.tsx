5aa548ba770816f4abcd75ac27bb59036b724585
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-10 22:34:07 -0500

    feat: enhance StateCard component with motion effects and additional props

--- src/components/pages/config/StateCard.tsx
+++ src/components/pages/config/StateCard.tsx
@@ -1,19 +1,21 @@
-import { BoardStatusProps } from "@/lib/types/global"
-import { DeleteIcon, EditIcon } from "@public/icon/Icon"
+import { DeleteIcon, DragIcon, EditIcon } from "@public/icon/Icon"
+import { StatusProps } from "@/lib/types/global"
 import Button from "@/components/ui/Button"
+import { motion } from "motion/react"
 
-export default function StateCard({ state }: { state: BoardStatusProps }) {
+export default function StateCard({ state, order }: { state: StatusProps, order: number }) {
     return (
-        <article style={{ backgroundColor: `${state.color}0f` }} className="hover:shadow-md transition-shadow flex justify-between items-center rounded-md text-sm group gap-2 p-4">
-            <aside className="flex justify-start items-center gap-2">
+        <motion.article className="border-button-secondary-border bg-button-secondary-background hover:shadow-md transition-shadow 
+        flex justify-between items-center rounded-md text-sm border group gap-2 p-6"
+            initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.2, delay: 0.25 * order }}>
+            <hgroup className="flex justify-start items-center gap-2">
                 <span style={{ backgroundColor: state.color }} className="flex justify-center items-center rounded-full p-1.5" />
-                <p style={{ color: state.color }} className="font-medium">{state.name}</p>
+                <h6 className="font-medium">{state.name}</h6>
+            </hgroup>
+            <aside style={{ color: state.color }} className="flex justify-end items-center group-hover:opacity-100 opacity-0 transition-opacity gap-2.5">
+                <Button variant="none" className="p-0!"><EditIcon size={20} /></Button>
+                <Button variant="none" className="p-0!"><DeleteIcon size={20} /></Button>
             </aside>
-
-            <aside style={{ color: state.color }} className="group-hover:opacity-100 transition-opacity opacity-0 flex justify-end items-center gap-2">
-                <Button variant="none" className="p-0!"><EditIcon size={16} /></Button>
-                <Button variant="none" className="p-0!"><DeleteIcon size={16} /></Button>
-            </aside>
-        </article>
+        </motion.article>
     )
 }
