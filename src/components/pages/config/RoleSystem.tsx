04ee17fbc1aa2bf0005a78b18eaf2c563ff788fd
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-16 18:13:38 -0500

    feat: implement RoleSystem component with role loading and display functionality

--- src/components/pages/config/RoleSystem.tsx
+++ src/components/pages/config/RoleSystem.tsx
@@ -1,17 +1,62 @@
-import { LockShieldIcon } from "@public/icon/Icon"
+import { useConfigStore } from "@/lib/shared/stores/ConfigStore"
+import { getAllRoles } from "@/lib/core/services/config.service"
+import { AddRoleIcon, LockShieldIcon } from "@public/icon/Icon"
+import { logger } from "@/lib/types/Logger"
+import { useEffect } from "react"
+import Button from "@/components/ui/Button"
+import RoleCard from "./comps/RoleCard"
 
 export default function RoleSystem() {
+    const { listRoles, setListRoles, isLoading, setLoading, error, setError, clearError } = useConfigStore()
+
+    const loadData = async () => {
+        try {
+            setLoading(true)
+            clearError()
+
+            const response = await getAllRoles()
+
+            if (response) setListRoles(response)
+            else setError('No se pudieron cargar el listado de roles')
+        } catch (error) {
+            logger.error('Error loading roles:', error)
+            setError('Error al cargar el listado de roles')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => { loadData() }, [])
     return (
-        <section className="bg-button-secondary-background flex justify-between items-center rounded-md shadow-md gap-2 p-6">
-            <aside className="flex items-center gap-4">
-                <span className="bg-orange-100 text-orange-600 flex justify-center items-center rounded-md aspect-square p-2">
-                    <LockShieldIcon />
-                </span>
-                <span className="flex flex-col gap-1">
-                    <h5 className="font-semibold text-xl">Roles y Permisos</h5>
-                    <p className="text-primary-border text-sm">Aquí puedes gestionar los roles y permisos de los usuarios del sistema.</p>
-                </span>
-            </aside>
-        </section>
+        <main className="bg-button-secondary-background rounded-md shadow-md flex flex-col">
+            <header className="flex justify-between items-center gap-2 p-6">
+                <aside className="flex items-center gap-4">
+                    <span className="bg-orange-100 text-orange-600 flex justify-center items-center rounded-md aspect-square p-2">
+                        <LockShieldIcon />
+                    </span>
+                    <hgroup className="flex flex-col gap-1">
+                        <h5 className="font-semibold text-xl">Roles y Permisos</h5>
+                        <p className="text-primary-border text-sm">Aquí puedes gestionar los roles y permisos de los usuarios del sistema.</p>
+                    </hgroup>
+                </aside>
+
+                <Button variant="primary" className="flex items-center gap-2">
+                    <AddRoleIcon size={16} strokeWidth={1.75} />
+                    Añadir Rol
+                </Button>
+            </header>
+
+            <hr className="border-button-secondary-border/25" />
+
+            <section className="flex flex-col gap-2 p-6">
+                {
+                    listRoles.length > 0 &&
+                    <span className="text-primary-border text-sm">En total hay {listRoles.length} roles {listRoles.length > 1 ? "configurados" : "configurado"}</span>
+                }
+
+                {/* Data state */}
+                {(!isLoading && !error && listRoles) && listRoles.map((role, id) => <RoleCard key={id} role={role} order={id} />)}
+            </section>
+        </main>
     )
 }
---
+++ src/components/pages/config/comps/RoleCard.tsx
@@ -0,0 +1,33 @@
+import { DeleteIcon, EditIcon, LockShieldIcon } from "@public/icon/Icon"
+import { RoleProps } from "@/lib/types/config"
+import Button from "@/components/ui/Button"
+import { motion } from "motion/react"
+
+export default function RoleCard({ role, order }: { role: RoleProps, order: number }) {
+    return (
+        <motion.article className="border-button-secondary-border bg-button-secondary-background hover:shadow-md transition-shadow 
+        flex justify-between items-center rounded-md text-sm border group gap-2 p-6"
+            initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.2, delay: 0.25 * order }}>
+            <aside className="flex flex-col">
+                <hgroup className="flex justify-start items-center gap-2">
+                    <span className="bg-orange-100 text-orange-600 flex justify-center items-center rounded-md aspect-square p-2"><LockShieldIcon /></span>
+                    <span className="flex flex-col">
+                        <h6 className="font-medium text-lg/tight">{role.name}</h6>
+                        <p className="text-primary-border text-sm">{role.permissions.length === 0 ? "Ningún" : role.permissions.length}&nbsp;
+                            {role.permissions.length <= 1 ? "permiso asignado" : "permisos asignados"}
+                        </p>
+                    </span>
+                </hgroup>
+
+                <div className="flex flex-wrap gap-1 mt-2">
+                    {role.permissions.slice(0, 5).map(permission => <span key={permission.name} className="bg-secondary text-button-secondary-text rounded-full text-xs px-2 py-1">{permission.name}</span>)}
+                    {role.permissions.length > 5 && <span className="bg-button-primary-background text-primary-border rounded-full text-xs px-2 py-1">+{role.permissions.length - 5} más</span>}
+                </div>
+            </aside>
+            <aside className="text-orange-600 flex justify-end items-center group-hover:opacity-100 opacity-0 transition-opacity gap-2.5">
+                <Button variant="none" className="p-0!"><EditIcon size={20} /></Button>
+                <Button variant="none" className="p-0!"><DeleteIcon size={20} /></Button>
+            </aside>
+        </motion.article>
+    )
+}
