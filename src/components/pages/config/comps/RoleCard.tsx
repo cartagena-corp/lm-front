58ba56d002b8578c5a516653a7b919e914b9da32
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-16 18:15:28 -0500

    fix: adjust spacing in RoleCard component for improved layout

--- src/components/pages/config/comps/RoleCard.tsx
+++ src/components/pages/config/comps/RoleCard.tsx
@@ -8,7 +8,7 @@ export default function RoleCard({ role, order }: { role: RoleProps, order: numb
         <motion.article className="border-button-secondary-border bg-button-secondary-background hover:shadow-md transition-shadow 
         flex justify-between items-center rounded-md text-sm border group gap-2 p-6"
             initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.2, delay: 0.25 * order }}>
-            <aside className="flex flex-col">
+            <aside className="flex flex-col gap-3">
                 <hgroup className="flex justify-start items-center gap-2">
                     <span className="bg-orange-100 text-orange-600 flex justify-center items-center rounded-md aspect-square p-2"><LockShieldIcon /></span>
                     <span className="flex flex-col">
@@ -21,7 +21,7 @@ export default function RoleCard({ role, order }: { role: RoleProps, order: numb
 
                 {
                     role.permissions.length > 0 &&
-                    <div className="flex flex-wrap gap-1 mt-2">
+                    <div className="flex flex-wrap gap-1">
                         {role.permissions.slice(0, 5).map(permission => <span key={permission.name} className="bg-secondary text-button-secondary-text rounded-full text-xs px-2 py-1">{permission.name}</span>)}
                         {role.permissions.length > 5 && <span className="bg-button-primary-background text-primary-border rounded-full text-xs px-2 py-1">+{role.permissions.length - 5} más</span>}
                     </div>
