e819d35ea12f1cab25f1178f3c477be12eabfadc
Kenn Marcucci <marcucci5783@gmail.com>
2025-08-29 23:05:32 -0500

    feat: add ChevronRightIcon component to Icon library

--- src/assets/Icon.tsx
+++ src/assets/Icon.tsx
@@ -325,10 +325,15 @@ export const FactoryIcon = ({ size = 24, stroke = 2 }: IconProps) => {
    )
 }
 
-
 export const KeyIcon = ({ size = 24, stroke = 1.5 }: IconProps) => (
    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={"none"} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={stroke}>
       <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" />
       <circle cx="16.5" cy="7.5" r=".5" fill="currentColor" />
+   </svg>
+)
+
+export const ChevronRightIcon = ({ size = 24, stroke = 1.5 }: IconProps) => (
+   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={"none"} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={stroke}>
+      <path d="m9 18 6-6-6-6" />
    </svg>
 )
